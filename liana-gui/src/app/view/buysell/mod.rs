20a28babb60d1951aceb9aa4eaf1d2398921e681
sokorototo <nyachiengatoto@gmail.com>
2025-11-16 00:03:57 +0300

    completed Mavapay client reform
    clear compilation issues
    
    use static typing for Mavapay form inputs
    
    reworked mavapay buysell message structure
    
    move all mavapay specific inputs outside of BuySellPanelState
    
    deprecate cached geolocator
    
    incremental improvements to mavapay ui
    
    deprecate business paths for mavapay
    
    completed major mavapay code rework
    
    move webview rendering logic into buysell flow state
    
    moved mavapay ui out of panel.rs

--- liana-gui/src/app/view/buysell/mod.rs
+++ liana-gui/src/app/view/buysell/mod.rs
@@ -1,5 +1,6 @@
 pub mod flow_state;
 pub mod panel;
+mod mavapay_ui;
 
-pub use flow_state::{BuySellFlowState, MavapayFlowState, NativePage};
-pub use panel::BuySellPanel;
+pub use flow_state::{MavapayFlowStep, MavapayState};
+pub use panel::{BuySellFlowState, BuySellPanel};
